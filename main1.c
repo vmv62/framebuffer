ed1c771cf2863de51002a890c384fa89024e7e56
vmv62 <vmv@nxt.ru>
2018-11-08 11:51:12 +0300

    first

---
+++ main1.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+
+int main(int argc, char* argv[])
+{
+  int fbfd = 0;
+  struct fb_var_screeninfo vinfo;
+  struct fb_fix_screeninfo finfo;
+  long int screensize = 0;
+  char *fbp = 0;
+
+	//Open bitmap file 
+	int bitmap = open("./1.bmp", O_RDWR);
+
+
+  // Open the framebuffer file for reading and writing
+  fbfd = open("/dev/fb0", O_RDWR);
+  if (!fbfd) {
+    printf("Error: cannot open framebuffer device.\n");
+    return(1);
+  }
+  printf("The framebuffer device was opened successfully.\n");
+
+  // Get fixed screen information
+  if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo)) {
+    printf("Error reading fixed information.\n");
+  }
+
+  // Get variable screen information
+  if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
+    printf("Error reading variable information.\n");
+  }
+  printf("%d\t%dx%d, %d bpp\n", finfo.smem_len, vinfo.xres, vinfo.yres, 
+         vinfo.bits_per_pixel );
+
+	printf("Bit per pixel: ", vinfo.bits_per_pixel);
+  // map framebuffer to user memory 
+  screensize = finfo.smem_len;
+
+  fbp = (char*)mmap(0, 
+                    screensize, 
+                    PROT_READ | PROT_WRITE, 
+                    MAP_SHARED, 
+                    fbfd, 0);
+
+  if ((int)fbp == -1) {
+    printf("Failed to mmap.\n");
+  }
+  else {
+    // draw...
+	
+  }
+
+	// cleanup
+	munmap(fbp, screensize);
+	close(fbfd);
+	close(bitmap);
+	return 0;
+}
---
+++ main2.c
@@ -0,0 +1,95 @@
+/*
+To test that the Linux framebuffer is set up correctly, and that the device permissions
+are correct, use the program below which opens the frame buffer and draws a gradient-
+filled red square:
+retrieved from:
+Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
+http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+int set_pixel(char *area, int x, int y, int color);
+
+int main()
+{
+    int fbfd = 0;
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    long int screensize = 0;
+    char *fbp = 0;
+    int x = 0, y = 0;
+    long int location = 0;
+
+    // Open the file for reading and writing
+    fbfd = open("/dev/fb0", O_RDWR);
+    if (fbfd == -1) {
+        perror("Error: cannot open framebuffer device");
+        exit(1);
+    }
+    printf("The framebuffer device was opened successfully.\n");
+
+    // Get fixed screen information
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+        perror("Error reading fixed information");
+        exit(2);
+    }
+
+    // Get variable screen information
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+        perror("Error reading variable information");
+        exit(3);
+    }
+
+    printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+
+    // Figure out the size of the screen in bytes
+    screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+
+    // Map the device to memory
+    fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+    if ((int)fbp == -1) {
+        perror("Error: failed to map framebuffer device to memory");
+        exit(4);
+    }
+    printf("The framebuffer device was mapped to memory successfully.\n");
+
+    printf("Offset x: %d\n", vinfo.xoffset);
+    x = 100; y = 100;       // Where we are going to put the pixel
+
+    // Figure out where in memory to put the pixel
+    for (y = 50; y < 300; y++)
+        for (x = 50; x < 300; x++) {
+
+            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+                       (y+vinfo.yoffset) * finfo.line_length;
+
+            if (vinfo.bits_per_pixel == 32) {
+                *(fbp + location) = 100;        // Some blue
+                *(fbp + location + 1) = 255;     // A little green
+                *(fbp + location + 2) = 176;    // A lot of red
+                *(fbp + location + 3) = 250;      // No transparency
+        //location += 4;
+            } else  { //assume 16bpp
+                int b = 10;
+                int g = (x-100)/6;     // A little green
+                int r = 31-(y-100)/16;    // A lot of red
+                unsigned short int t = r<<11 | g << 5 | b;
+                *((unsigned short int*)(fbp + location)) = t;
+            }
+
+        }
+    munmap(fbp, screensize);
+    close(fbfd);
+    return 0;
+}
+
+int set_pixel(char *area, int x, int y, int color){
+	int lines = (y - 1) * 
+ }
