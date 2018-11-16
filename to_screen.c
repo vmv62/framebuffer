9178b892a6b8fb21e9baaa4bcfd5504471af2d2e
vmv62 <vmv@nxt.ru>
2018-11-16 10:44:50 +0300

    Small changes

--- to_screen.c
+++ to_screen.c
@@ -2,8 +2,10 @@
 To test that the Linux framebuffer is set up correctly, and that the device permissions
 are correct, use the program below which opens the frame buffer and draws a gradient-
 filled red square:
+
 retrieved from:
 Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
+
 http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
 */
 
@@ -14,8 +16,6 @@ http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
-
-int set_pixel(char *area, int x, int y, int color);
 
 int main()
 {
@@ -60,36 +60,26 @@ int main()
     }
     printf("The framebuffer device was mapped to memory successfully.\n");
 
-    printf("Offset x: %d\n", vinfo.xoffset);
-    x = 100; y = 100;       // Where we are going to put the pixel
+   x = 400; y = 500;       // Координаты начала изображения.
+   int xp = 100, yp = 100;     // Размер выводимого изображения.
 
-    // Figure out where in memory to put the pixel
-    for (y = 50; y < 300; y++)
-        for (x = 50; x < 300; x++) {
+   int cur = ((vinfo.xres * 4)*(y-1)) + (x*4);
 
-            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                       (y+vinfo.yoffset) * finfo.line_length;
+   for(int i =0; i < screensize; i++){
+      *(fbp + i) = 0x00;
+   }
 
-            if (vinfo.bits_per_pixel == 32) {
-                *(fbp + location) = 100;        // Some blue
-                *(fbp + location + 1) = 255;     // A little green
-                *(fbp + location + 2) = 176;    // A lot of red
-                *(fbp + location + 3) = 250;      // No transparency
-        //location += 4;
-            } else  { //assume 16bpp
-                int b = 10;
-                int g = (x-100)/6;     // A little green
-                int r = 31-(y-100)/16;    // A lot of red
-                unsigned short int t = r<<11 | g << 5 | b;
-                *((unsigned short int*)(fbp + location)) = t;
-            }
+   fbp += cur;
 
-        }
+   for(int i=0; i < xp; i++){
+      *(fbp + 0) = 0xff;
+      *(fbp + 1) = 0xff;
+      *(fbp + 2) = 0xff;
+      fbp += 4;
+   }
+
+
     munmap(fbp, screensize);
     close(fbfd);
     return 0;
 }
-
-int set_pixel(char *area, int x, int y, int color){
-	int lines = (y - 1) * 
- }
