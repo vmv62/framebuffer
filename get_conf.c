e189d5a7a1c54539f63119473b4399dd9798d6a8
vmv62 <vmv@nxt.ru>
2018-12-21 16:16:10 +0300

    Auto commit

--- get_conf.c
+++ get_conf.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "get_conf.h"
 #include "bmp.h"
 
 int main(int argc, char **argv){
 	sconf_t *conf;
+	prg_dat_t *p_conf;
 
 	conf = (sconf_t *)malloc(sizeof(sconf_t));
-	read_conf(argv[1], conf);
+	p_conf = (prg_dat_t *)malloc(sizeof(prg_dat_t));
+	read_conf(argv[1], conf, p_conf);
+
+	printf("Object count == %d\n", p_conf->obj_count);
+
+	free(conf);
+	free(p_conf);
 	return 0;
 }
 
-uint32_t read_conf(char *file, sconf_t *conf){
+uint32_t read_conf(char *file, sconf_t *conf, prg_dat_t *p_conf){
 	FILE *fd;
 	char buff[BUFF_LEN];
 
@@ -24,15 +32,42 @@ uint32_t read_conf(char *file, sconf_t *conf){
 	int i = 0;
 	while(NULL != fgets(buff, BUFF_LEN, fd)){
 		i++;
-		parse_string(buff, files[i]);
+		parse_string(buff, conf);
+		if(0 != is_a_object(buff, conf)){
+			p_conf->obj_count++;
+		}
+
 	}
 
+	printf("obj_name - %s\n", conf->obj_name);
 	fclose(fd);
 	return 0;
 }
 
-uint32_t parse_string(char *string, sconf_t pict){
-
+uint32_t parse_string(char *string, sconf_t *pict){
 	printf("%s\n", string);
+	return 0;
+}
+
+
+//Счетчик объектов в конфигурационном файле.
+uint32_t is_a_object(char *string, sconf_t *pict){
+	char *open_brace = 0, *close_brace = 0, *cursor = string;
+
+	while(*cursor != (0 | '\n')){
+		if(*cursor == '['){
+			open_brace = cursor;
+		}
+		if(*cursor == ']'){
+			close_brace = cursor;
+		}
+		cursor++;
+	}
+
+	if(open_brace < close_brace){
+		memcpy(pict->obj_name, open_brace + 1, close_brace - string - 1);
+		return close_brace - open_brace;
+	}
+
 	return 0;
 }
--- get_conf.h
+++ get_conf.h
@@ -2,6 +2,10 @@
 
 #define BUFF_LEN 100
 
+typedef struct{
+	int obj_count;
+}prg_dat_t;
+
 //Show modes
 enum{
 	BLINK = 1,
@@ -11,15 +15,17 @@ enum{
 };
 
 typedef struct{
-	uint8_t file_name_1[50];
-	uint8_t file_name_2[50];
+	uint8_t file_name_1[64];
+	uint8_t file_name_2[64];
 	uint8_t show_mode;
 	uint8_t reserved;
+	char obj_name[64];
 	uint32_t xcoord;
 	uint32_t ycoord;
 }sconf_t;
 
 sconf_t files[20];
 
-uint32_t read_conf(char *file, sconf_t *conf);
-uint32_t parse_string(char *string, sconf_t pict);
+uint32_t read_conf(char *file, sconf_t *pict, prg_dat_t *p_conf);			//Читаем конфигурационный файл.
+uint32_t is_a_object(char *string, sconf_t *pict);							//Определяем заголовок объекта
+uint32_t parse_string(char *string, sconf_t *pict);		//Парсим строку, Заполняя парасетры в структуре
