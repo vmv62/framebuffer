d8d3de5828e9db60024f72a13f1467dfed218a8c
vmv62 <vmv@nxt.ru>
2018-12-23 16:42:26 +0300

    Auto commit

--- get_conf.c
+++ get_conf.c
@@ -32,20 +32,34 @@ uint32_t read_conf(char *file, sconf_t *conf, prg_dat_t *p_conf){
 	int i = 0;
 	while(NULL != fgets(buff, BUFF_LEN, fd)){
 		i++;
-		parse_string(buff, conf);
-		if(0 != is_a_object(buff, conf)){
+		if(is_a_object(buff, conf)){
 			p_conf->obj_count++;
+		}else{
+			parse_string(buff, conf);
 		}
 
 	}
-
 	printf("obj_name - %s\n", conf->obj_name);
 	fclose(fd);
 	return 0;
 }
 
 uint32_t parse_string(char *string, sconf_t *pict){
-	printf("%s\n", string);
+	char *key_pos = string, *cursor;
+	char key[64], argument[64];
+	while(*key_pos){
+		if(*key_pos == '='){
+//			key_pos = string;
+			break;
+		}
+		key_pos++;
+	}
+	printf("%d", string - key_pos - 1);
+	memcpy(key, string, string - key_pos - 1);
+//	memcpy(argument, key_pos, strlen(string) - (key_pos - string));
+
+	printf("Key: %s\n", key);
+//	printf("Argument: %s\n", argument);
 	return 0;
 }
 
@@ -66,8 +80,17 @@ uint32_t is_a_object(char *string, sconf_t *pict){
 
 	if(open_brace < close_brace){
 		memcpy(pict->obj_name, open_brace + 1, close_brace - string - 1);
-		return close_brace - open_brace;
+		return close_brace - string - 1;
 	}
 
 	return 0;
 }
+/*
+char *clear_string(char *in_string){
+	int str_len;
+	str_len = strlen(string);
+	char tmp_str[str_len];
+
+
+}
+*/
--- get_conf.h
+++ get_conf.h
@@ -29,3 +29,4 @@ sconf_t files[20];
 uint32_t read_conf(char *file, sconf_t *pict, prg_dat_t *p_conf);			//Читаем конфигурационный файл.
 uint32_t is_a_object(char *string, sconf_t *pict);							//Определяем заголовок объекта
 uint32_t parse_string(char *string, sconf_t *pict);		//Парсим строку, Заполняя парасетры в структуре
+char *clear_string(char *in_string);
