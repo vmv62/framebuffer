e079f0e2097d04117b25d8ee9308b882e34a4989
vmv62 <vmv@nxt.ru>
2018-12-25 13:00:30 +0300

    Auto commit

--- get_conf.c
+++ get_conf.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
 	p_conf = (prg_dat_t *)malloc(sizeof(prg_dat_t));
 	read_conf(argv[1], conf, p_conf);
 
-	printf("Object count == %d\n", p_conf->obj_count);
+//	printf("Object count == %d\n", p_conf->obj_count);
 
 	free(conf);
 	free(p_conf);
@@ -45,8 +45,11 @@ uint32_t read_conf(char *file, sconf_t *conf, prg_dat_t *p_conf){
 }
 
 uint32_t parse_string(char *string, sconf_t *pict){
-	char *key_pos = string, *cursor;
+	char *key_pos, *cursor;
 	char key[64], argument[64];
+
+	key_pos = clear_string(string);
+	printf("%s", key_pos);
 	while(*key_pos){
 		if(*key_pos == '='){
 //			key_pos = string;
@@ -54,11 +57,11 @@ uint32_t parse_string(char *string, sconf_t *pict){
 		}
 		key_pos++;
 	}
-	printf("%d", string - key_pos - 1);
+//	printf("%d", string - key_pos - 1);
 	memcpy(key, string, string - key_pos - 1);
 //	memcpy(argument, key_pos, strlen(string) - (key_pos - string));
 
-	printf("Key: %s\n", key);
+//	printf("Key: %s\n", key);
 //	printf("Argument: %s\n", argument);
 	return 0;
 }
@@ -66,7 +69,9 @@ uint32_t parse_string(char *string, sconf_t *pict){
 
 //Счетчик объектов в конфигурационном файле.
 uint32_t is_a_object(char *string, sconf_t *pict){
-	char *open_brace = 0, *close_brace = 0, *cursor = string;
+	char *open_brace = 0, *close_brace = 0,
+	*cursor = clear_string(string);
+
 
 	while(*cursor != (0 | '\n')){
 		if(*cursor == '['){
@@ -78,6 +83,10 @@ uint32_t is_a_object(char *string, sconf_t *pict){
 		cursor++;
 	}
 
+	if((open_brace && close_brace) == 0){
+		return 0;
+	}
+
 	if(open_brace < close_brace){
 		memcpy(pict->obj_name, open_brace + 1, close_brace - string - 1);
 		return close_brace - string - 1;
@@ -85,13 +94,28 @@ uint32_t is_a_object(char *string, sconf_t *pict){
 
 	return 0;
 }
-/*
+
+//Чистит строку от пробелов.
 char *clear_string(char *in_string){
+	char *in_ptr, *b_ptr;
 	char buffer[200];
+	int char_cntr = 0;
 
-	while(){
-		
+	in_ptr = in_string;
+	b_ptr = buffer;
+	while(*in_ptr){
+		if(*in_ptr != ' '){
+			*b_ptr = *in_ptr;
+			b_ptr++;
+			in_ptr++;
+			char_cntr++;
+		}else{
+			in_ptr++;
+			}
 	}
-
+	b_ptr++;
+	*b_ptr = 0;
+	memcpy(in_string, buffer, char_cntr);
+	return in_string;
 }
-*/
+
