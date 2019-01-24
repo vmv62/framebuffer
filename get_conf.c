030f83c3685fb3a2de75d1e264bb6ab9773cf28d
vmv62 <vmv@nxt.ru>
2019-01-24 11:47:51 +0300

    Исправленны ошибки с размещением данных в структурах. Проблеммы с точностью записи полей структуры для конкретного объекта

--- get_conf.c
+++ get_conf.c
@@ -9,9 +9,15 @@ int main(int argc, char **argv){
 	prg_dat_t *p_conf;
 
 	p_conf = (prg_dat_t *)malloc(sizeof(prg_dat_t));
-//	printf("%d\n", sizeof(p_conf));
 	read_conf("monitor.conf", p_conf);
 
+	printf("%s\n", p_conf->object[0]->obj_name);
+	printf("%s\n", p_conf->object[1]->obj_name);
+//	printf("%d\n", p_conf->object[1]->xcoord);
+
+	for(int i = 0; i < p_conf->obj_count; i++){
+		free(p_conf->object[i]);
+	}
 	free(p_conf);
 	return 0;
 }
@@ -20,22 +26,19 @@ int main(int argc, char **argv){
 uint32_t read_conf(char *file, prg_dat_t *p_conf){
 	FILE *fd;
 	char buff[BUFF_LEN];
+	int obj_cnt = 0;
 
 	if(NULL == (fd = fopen(file, "r"))){
 		printf("Error file open\n");
 	}
 
-//	printf("Read_conf before while\n");
 	//Перебор строк файла с настройками и извлечение необходимой информации из них.
 	while(NULL != fgets(buff, BUFF_LEN, fd)){
 		if(is_a_object(buff, p_conf)){
-			p_conf->object[p_conf->obj_count] = (oconf_t *)malloc(sizeof(oconf_t));
-			p_conf->obj_count++;
-//			printf("obj count = %d\n", p_conf->obj_count);
+			p_conf->object[obj_cnt] = (oconf_t *)malloc(sizeof(oconf_t));
+			obj_cnt++;
 		}else{
-//			printf("%s", "Вывод перед парсингом параметров.");
-			parse_string(buff, p_conf->object[p_conf->obj_count]);
-//			printf("Obj_cntr %d\n", p_conf->obj_count);
+			parse_string(buff, p_conf->object[obj_cnt - 1]);
 		}
 
 	}
@@ -58,38 +61,26 @@ uint32_t parse_string(char *string, oconf_t *pict){
 		cursor++;
 	}
 
-	printf("%d\n", key_pos - string);
+//	printf("%d\n", key_pos - string);
 	memcpy(key, string, (key_pos - string));
 	key[key_pos - string] = 0;
 	strcpy(argument, key_pos+1);
-	printf("%s\n", key);
-	printf("%s", argument);
 	if(!strcmp(key, "image_1")){
-//		printf("%s\n", "Copy string to struct");
-//		strcpy(pict->file_name_1, argument);
-//		argument[cursor - key_pos] = 0;
-//		printf("%d\n", pict->obj_count);
-//		strcpy(pict->file_name_1, argument);
-//		printf("%s\n", "Print value from struct");
-//		printf("%s", pict->file_name_1);
+		strcpy(pict->file_name_1, argument);
 	}
-/*
+
 	if(!strcmp(key, "image_2")){
-		memcpy(argument, key_pos, cursor - key_pos);
-		argument[cursor - key_pos] = 0;
-		printf("%s", argument);
+		strcpy(pict->file_name_2, argument);
 	}
 
 	if(!strcmp(key, "xcoord")){
-		pict->object[pict->obj_count]->xcoord = atoi(key_pos);
-		printf("%d\n", pict->object[pict->obj_count]->xcoord);
+		pict->xcoord = atoi(argument);
 	}
 
 	if(!strcmp(key, "ycoord")){
-		pict->object[pict->obj_count]->ycoord = atoi(key_pos);
-		printf("%d\n", pict->object[pict->obj_count]->ycoord);
+		pict->ycoord = atoi(argument);
 	}
-*/
+
 	return 0;
 }
 
@@ -119,7 +110,8 @@ uint32_t is_a_object(char *string, prg_dat_t *pict){
 	if(open_brace < close_brace){
 		//размещаем указатель в массив объектов.
 		pict->object[pict->obj_count] = (oconf_t *)malloc(sizeof(oconf_t));
-		memcpy(pict->object[0]->obj_name, open_brace + 1, close_brace - string - 1);
+		memcpy(pict->object[pict->obj_count]->obj_name, open_brace + 1, close_brace - string - 1);
+		pict->obj_count++;
 		return close_brace - string - 1;
 	}
 
